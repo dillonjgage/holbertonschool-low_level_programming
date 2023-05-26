e870fee383df0dc8ba01f9986981633b0cb1baf4
dillonjgage <dillonjgage1@gmail.com>
2023-05-26 11:49:02 -0700

    print type size

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -1,21 +1,25 @@
 #include <stdio.h>
 /**
  * main - prints the size of specific types
- * 
+ *
  * Return 0 if successful
  */
 int main(void)
 {
-	int intType;
-	float floatType;
-	char charType;
-	long long int lliType;
-	long int liType;
+	/**
+	 * int intType;
+	 * float floatType;
+	 * char charType;
+	 * long long int lliType;
+	 * long int liType;
+	 * Did the above, but it is not necessary to set types as variables.
+	 * Calling on the types below is sufficient enough
+	 */
 
-        printf("Size of a char: %zu byte(s)\n", sizeof(charType));
-	printf("Size of an int: %zu byte(s)\n", sizeof(intType));
-        printf("Size of a long int: %zu byte(s)\n", sizeof(liType));
-        printf("Size of a long long int: %zu byte(s)\n", sizeof(lliType));
-        printf("Size of a float: %zu byte(s)\n", sizeof(floatType));
+	printf("Size of a char: %zu byte(s)\n", sizeof(char));
+	printf("Size of an int: %zu byte(s)\n", sizeof(int));
+	printf("Size of a long int: %zu byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %zu byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %zu byte(s)\n", sizeof(float));
 	return (0);
 }
