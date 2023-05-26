faad5541a2c15de68260ee0e4810ee81b05db894
dillonjgage <dillonjgage1@gmail.com>
2023-05-25 20:12:33 -0700

    puts

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
-int main(void)/* C program using puts() */
+/**
+ *main - puts string
+ */
+int main(void)
 {
 	char Mystr[] = "\"Programming is like building a multilingual puzzle";
 
 	puts(Mystr);
-
 	return (0);
 }
