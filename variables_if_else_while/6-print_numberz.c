7584941e6d137b5c2a0baa9530e7a226895ea9c7
dillonjgage <dillonjgage1@gmail.com>
2023-05-28 14:54:05 -0700

    putchar for int using ASCII chart

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - prints single digit nums of base 10 starting at 0
+ * 0 is 48 for the ASCII characters, which allows us to us putchar
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 48; n <58; n++)
+	{
+		putchar(n);
+	}
+	putchar('\n');
+	return (0);
+}
