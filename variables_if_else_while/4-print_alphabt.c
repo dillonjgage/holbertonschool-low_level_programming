f10044f50291c04fab0c622b74dc2dc1007f373d
dillonjgage <dillonjgage1@gmail.com>
2023-05-28 12:34:48 -0700

    alpha without q and e

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - print the alphabet in lowercase and skip q and e
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char alpha = 'a';
+
+	while (alpha <= 'z')
+	{	
+		putchar(alpha);
+		alpha++;
+		
+		if (alpha = 'q' || alpha = 'e')
+		{
+			continue;
+		}
+	}
+	putchar('\n');
+	return (0);
+}
