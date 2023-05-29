be1cb5050efe69af2a21e2e11f3d326e8c563a5f
dillonjgage <dillonjgage1@gmail.com>
2023-05-29 14:56:15 -0700

    alpha x10

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+/**
+ * print_alphabet_x10 - print alphabet 10 times
+ *
+ * Return: 0 if successful
+ */
+#include "main.h"
+
+void print_alphabet_x10(void)
+{
+	int count = 0;
+	while (count < 10)
+	{
+
+		char alpha = 'a';
+
+		while (alpha <= 'z')
+		{
+			_putchar(alpha);
+			alpha++;
+		}
+		_putchar('\n');
+	}
+	count++;
+}
