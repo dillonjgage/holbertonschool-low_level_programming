e2e6f1fed6bc1bebc60d4a9be1ad4d036a55f245
dillonjgage <dillonjgage1@gmail.com>
2023-05-29 12:05:48 -0700

    test

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -5,29 +5,16 @@
  *Return: 0
  */
 #include"main.h"
+
 int main(void)
 {
-	/**
-	 * Declare an array of integers call 'aray' with a length of 9
-	 */
-	int array[9] = { '_','p','u','t','c','h','a','r','\n' };
-	/**
-	 * Declare and initialize a variable 'i' with the value 0
-	 */
+	char myputchar[] = "_putchar";
 	int i = 0;
-	/**
-	 * Start a while loop that continues until 'i' is greater than 9
-	 */
 	while (i <= 9)
 	{
-		/**
-		 * Print the character at the 'i'-th index of the 'array'
-		 * putchar(array[i]);
-		 *
-		 * Increment 'i' by 1
-		 */
-		putchar(array[i]);
+		_putchar(myputchar[i]);
 		i++;
 	}
+	_putchar('\n');
 	return (0);
 }
--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -1,6 +1,7 @@
 #ifndef HEADER_F
 #define HEADER_F
+#include <stdio.h>
 
-
+int _putchar(char c);
 
 #endif
