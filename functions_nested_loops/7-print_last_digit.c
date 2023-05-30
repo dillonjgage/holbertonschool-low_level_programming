723ba37588078b66f47f6c6fa1815da03720eb51
dillonjgage <dillonjgage1@gmail.com>
2023-05-29 17:07:22 -0700

    last digit num % 10

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * print_last_digit - print the last digit of a number
+ * @num: number as input
+ *
+ * Return: rem - the remainder of 10 - aka last digit
+ */
+int print_last_digit(int num)
+{
+	int rem = num % 10;
+	return (rem);
+}
