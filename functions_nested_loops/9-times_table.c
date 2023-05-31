7f51f409dbd87f70b82f29263778bc5cdc97e79a
dillonjgage <dillonjgage1@gmail.com>
2023-05-30 19:03:55 -0700

    9 times table

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -6,11 +6,11 @@
  */
 void times_table(void)
 {
-	for(int i = 0; i < 10; i++)
+	for (int i = 0; i < 10; i++)
 	{
 		_putchar('0');
 
-		for(int mul = 0; mul < 10; mul++)
+		for (int mul = 0; mul < 10; mul++)
 		{
 			int mul_results = i * mul;
 			_putchar(mul_results);
