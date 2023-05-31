ab69427d79a3d518e0606c81e699368218452c93
dillonjgage <dillonjgage1@gmail.com>
2023-05-30 20:50:41 -0700

    9 times table

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -6,44 +6,47 @@
  */
 void times_table(void)
 {
-    int row = 0;
-    int col;
-
-    while (row <= 9)
-    {
-        col = 0;
-        while (col <= 9)
-        {
-            int result = row * col;
-
-            if (result >= 10 && col != 9)
-            {
-                _putchar((result / 10) + '0');
-                _putchar((result % 10) + '0');
-                _putchar(',');
-                _putchar(' ');
-            }
-            else if (result < 10 && col != 9)
-            {
-                _putchar(' ');
-		_putchar(result + '0');
-                _putchar(',');
-                _putchar(' ');
-            }
-	    else if (result >= 10 && col == 9)
-	    {
-		    _putchar((result / 10) + '0');
-		    _putchar((result % 10) + '0');
-	    }
-	    else
-            {
-                _putchar(result + '0');
-	    }
-
-	    col++;
-        }
-
-	_putchar('\n');
-        row++;
-    }
+	int row = 0;
+	int col;
+	
+	while (row <= 9)
+	{
+		col = 0;
+		
+		while (col <= 9)
+		{
+			int result = row * col;
+			
+			if (result >= 10 && col != 9)
+			{
+				_putchar((result / 10) + '0');
+				_putchar((result % 10) + '0');
+				_putchar(',');
+				_putchar(' ');
+			}
+			
+			else if (result < 10 && col != 9)
+			{
+				_putchar(' ');
+				_putchar(result + '0');
+				_putchar(',');
+				_putchar(' ');
+			}
+			
+			else if (result >= 10 && col == 9)
+			{
+				_putchar((result / 10) + '0');
+				_putchar((result % 10) + '0');
+			}
+			
+			else
+			{
+				_putchar(result + '0')
+			}
+			
+			col++;
+		}
+		_putchar('\n');
+		row++;
+	}
 }
