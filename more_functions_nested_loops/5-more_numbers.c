d4e68df2b3f3b4675276ed6c25a5fd124aa356dd
dillonjgage <dillonjgage1@gmail.com>
2023-06-02 14:45:09 -0700

    print 0 through 14 ten times

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ * prints nums 0 through 14 ten times
+ *
+ * Return: empty
+ */
+void more_numbers(void)
+{
+	int count;
+	int num;
+
+	for (count = 0; count < 10; count++)
+	{
+		for (num = 0; num < 15; num++)
+		{
+			int last = num % 10;
+			int first = num / 10;
+
+			if (num <= 9)
+			{
+				_putchar('0'+last);
+			}
+			else
+			{
+				_putchar('0'+first);
+				_putchar('0'+last);
+			}
+		}
+		_putchar('\n');
+	}
+}
