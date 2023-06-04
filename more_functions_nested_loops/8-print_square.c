0b52ebf2e63190520413c2c2f83b6dd6687bbd13
dillonjgage <dillonjgage1@gmail.com>
2023-06-04 12:59:20 -0700

    print a box

---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ * print_square - prints out a square based on the input size
+ * @size: input size 
+ *
+ * Return: empty
+ */
+void print_square(int size)
+{
+	if (size > 0)
+	{
+		int length = 1;
+
+		while (length <= size)
+		{
+			int width = 1;
+
+			while (width <= size)
+			{
+				_putchar(35);
+				width++;
+			}
+			_putchar('\n');
+			length++;
+		}
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
