7504a10b19e96ab3c20d93465ad0c932904acb4f
dillonjgage <dillonjgage1@gmail.com>
2023-06-09 16:51:10 -0700

    copy string by n index limit

---
+++ pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * _strncpy - copy a string
+ * @dest: destination of copied string
+ * @src: source string to be copied
+ * @n: index length to copy - a limit
+ *
+ * Return: updated dest
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i = 0;
+	int ii = 0;
+
+	while (ii <= n && src[ii] != 0)
+	{
+		dest[i] = src[ii];
+		i++;
+		ii++;
+	}
+	return (dest);
+}
