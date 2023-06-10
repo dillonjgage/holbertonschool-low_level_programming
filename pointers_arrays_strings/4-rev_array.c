bcda96fcd480c1ca4d4887a56de5af00e88cda31
dillonjgage <dillonjgage1@gmail.com>
2023-06-10 13:34:06 -0700

    reverse an array (its a triangle process)

---
+++ pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * reverse_array - reverse an array
+ * @a: pointer to original array
+ * @n: length of the array
+ *
+ * Return: void
+ */
+void reverse_array(int *a, int n)
+{
+	int temp;
+	int i = 0;
+
+	while (i < n)
+	{
+		temp = a[n - 1];
+		/** copied from a so it is safe
+		 */
+		a[n - 1] = a[i];
+		/**
+		 * a is copied over since it is safe
+		 */
+		a[i] = temp;
+		/** a grabs the copy from temp
+		 * its like a triagle!
+		 */
+		i++;
+		n--;
+	}
+}
