ff75882ffe8c3de95d20109b73ffac8c4870665c
dillonjgage <dillonjgage1@gmail.com>
2023-06-15 12:31:32 -0700

    print a string

---
+++ recursion/0-puts_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * prints a string via recursion
+ * @s: pointer to string to be printed
+ *
+ * Return: empty unless NULL, then new line
+ */
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		_putchar('\n');
+		return;
+	}
+
+	_putchar(*s);
+	_puts_recursion(s + 1);
+}
