e88762d425602f5b9541cd54e9700b6aa9f1add0
dillonjgage <dillonjgage1@gmail.com>
2023-06-25 13:21:17 -0700

    num of args

---
+++ argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * main - print the number of args passed
+ * @argc: arg count
+ * @argv: array of pointers to the args
+ *
+ * Return: 0
+ */
+int main(int argc, char __attribute__((__unused__)) *argv[])
+{
+	printf("%d\n", argc);
+
+	return (0);
+}
