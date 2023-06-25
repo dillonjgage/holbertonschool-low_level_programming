8dc60e5cb572818af310af3c9e7f43bdc3c4856d
dillonjgage <dillonjgage1@gmail.com>
2023-06-25 13:43:06 -0700

    print all args

---
+++ argc_argv/2-args.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * main - prints all args recieved
+ * @argc: arg count
+ * @argv: array of pointers to the args
+ *
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for(i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+
+	return (0);
+}
