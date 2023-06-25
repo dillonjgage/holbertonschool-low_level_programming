813b7aa1c5ce2fad0c315671cc5a1337ce475dcf
dillonjgage <dillonjgage1@gmail.com>
2023-06-25 14:04:44 -0700

    add positive numbers

---
+++ argc_argv/4-add.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * main - add two positive numbers
+ * @argc: arg count
+ * @argv: array of pointers for args
+ *
+ * Return: 0 - successfully add two positive numbers, else 1 for error
+ */
+int main(int argc, char *argv[])
+{
+	int num, digit, sum = 0;
+
+	for (num = 1; num < argc; num++)
+	{
+		for (digit = 0; argv[num][digit]; digit++)
+		{
+			if (argv[num][digit] < '0' || argv[num][digit] > '9')
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+
+		sum += atoi(argv[num]);
+	}
+
+	printf("%d\n", sum);
+
+	return (0);
+}
