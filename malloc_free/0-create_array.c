7bde46c723252a430faf22af56fc83f61d3ac167
dillonjgage <dillonjgage1@gmail.com>
2023-06-26 14:01:08 -0700

    create array

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -1,9 +1,30 @@
 #include "main.h"
 /**
+ * create_array - creates an array of chars with a specified char
+ * @size: specified size of desired array
+ * @c: specified char
  *
- *
- *
+ * Return: NULL if size == 0 OR str for completed array
  */
 char *create_array(unsigned int size, char c)
 {
+	char *str;
+	int len = size + 1;
+	int i;
 
+	str = malloc(sizeof(char) * len);
+
+	if (size == 0)
+	{
+		return (NULL);
+	}
+	else
+	{
+		for (i = 0; i < len; i++)
+		{
+			str[i] = c;
+		}
+	}
+	return (str);
+	free(str);
+}
--- malloc_free/main.h
+++ malloc_free/main.h
@@ -1,6 +1,7 @@
 #ifndef Main_H
 #define Main_H
 #include <stdio.h>
+#include <stdlib.h>
 
 char *create_array(unsigned int size, char c);
 char *_strdup(char *str);
