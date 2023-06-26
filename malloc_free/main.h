4847d4ba42dc8e49218446459b2591a3d776fee3
dillonjgage <dillonjgage1@gmail.com>
2023-06-26 13:24:30 -0700

    update prototypes

--- malloc_free/main.h
+++ malloc_free/main.h
@@ -1,7 +1,11 @@
 #ifndef Main_H
 #define Main_H
 #include <stdio.h>
-#include <stdlib.h>
 
+char *create_array(unsigned int size, char c);
+char *_strdup(char *str);
+char *str_concat(char *s1, char *s2);
+int **alloc_grid(int width, int height);
+void free_grid(int **grid, int height);
 
 #endif
