fb3b96cf0b0fb2d5b479c1654335ff6fc099f2cb
dillonjgage <dillonjgage1@gmail.com>
2023-07-01 12:20:26 -0700

    struct dog

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,11 +1,17 @@
 #ifndef DOG_H
 #define DOG_H
 
+/**
+ * struct dog - new type describing a dog
+ * @name: the dog's name
+ * @age: the dog's age
+ * @owner: the dog's owner
+ */
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-}
+};
 
 #endif
